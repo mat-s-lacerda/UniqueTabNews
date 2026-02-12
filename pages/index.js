172d1bac99bd96685c305debe32c857fbef658ba
MSLacerda <matheus_lacerda_2@carrefour.com>
2026-02-11 22:31:02 -0300

    [add] adicionar

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
 function Home() {
-    return (
-        <div>
-            <h1>Você me tirou uma esfiha!!!</h1>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Você me tirou uma esfiha!!!</h1>
+    </div>
+  );
 }
 
 export default Home;
